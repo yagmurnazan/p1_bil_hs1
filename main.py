ce2ada55f190aad1df26db223243ff205c804437
yagmurnazan <147739421+yagmurnazan@users.noreply.github.com>
2023-10-13 16:42:40 +0000

    if elif else öğrenimi

--- main.py
+++ main.py
@@ -1,13 +1,83 @@
-# print('Hello World')
-#print('Merhaba Dünya\nMerhaba Dünya\nHello World\n')
-# print('Hello'+ ' ' +'Yağmur')
-# print( input('Adın Nedir?'))
-# print('Merhaba' + ' ' + input('Adın Ne?'))
-# # print(len(input('Hello Whats Your Name?'
-# Name = ('Yağmur')
-# print(Name)
-# Name = ('Nazan')
+# # print('Hello World')
+# #print('Merhaba Dünya\nMerhaba Dünya\nHello World\n')
+# # print('Hello'+ ' ' +'Yağmur')
+# # print( input('Adın Nedir?'))
+# # print('Merhaba' + ' ' + input('Adın Ne?'))
+# # # print(len(input('Hello Whats Your Name?'
+# # Name = ('Yağmur')
 # # print(Name)
-isim = input('Hello Whats Your Name?')
-uzunluk = len(isim)
-print(uzunluk)
+# print('Hello World')
+# # Name = ('Nazan')
+# # # print(Name)
+# # isim = input('Hello Whats Your Name?')
+# # uzunluk = len(isim)
+# # print(uzunluk)
+# # print(input('Hello Whats Your Name?'))
+# # ensongidilenşehir = input('en son gittiğiniz şehir nedir?')
+# # ensevdiğinhayvan = input('en sevdiğiniz hayvanın adı nedir')
+# # # print('Grup Adı: '+' '+ ensongidilenşehir + ensevdiğinhayvan)
+# sayı1 = input('1inci sayıyı giriniz:')
+# sayı2 = input('2nci sayıyı giriniz:')
+# toplam = int(sayı1) + int(sayı2)
+# # print('Toplam =' + str(toplam))
+# kısakenar = int(input('kısa kenarı giriniz:'))
+# uzunkenar = int(input('uzun kenarı giriniz:'))
+# # Çevre = (kısakenar + uzunkenar) *2
+# # Alan = kısakenar * uzunkenar
+# # print('Dikdörtgenin çevresi:' + str(Çevre))
+# # print('Dikdörtgenin alanı:' + str(Alan))
+# Yarıçap = int(input('Yarıçapı Giriniz'))
+# Yükseklik = int(input('Yüksekliği Giriniz'))
+# Pİsayısı = 3.14
+# TabanAlanı = Pİsayısı * Yarıçap * Yarıçap
+# TabanÇevresi = 2* Pİsayısı * Yarıçap
+# YanalAlan = Yükseklik * TabanÇevresi
+# ToplamAlan = (2*TabanAlanı) * YanalAlan
+# Hacim = TabanAlanı * Yükseklik
+# print('Silindirin hacmi: ' + str(Hacim))
+# print('Silindirin alanı: '+ str(ToplamAlan))
+# Yarıçap = int(input('Yarıçapı Giriniz'))
+# Pİsayısı = 3.14
+# Çevre = 2* Pİsayısı * Yarıçap
+# Hacim = Pİsayısı * Yarıçap * Yarıçap
+# print('Dairenin Hacmi:' + str(Hacim))
+# # print('Dairenin Çevresi: ' + str (Çevre))
+# # print('Çek Cumhuriyeti'[4])
+# İsimUzunluk = len(input('Merhaba, Adın Ne?'))
+# # isimUzunluk_Str = str(İsimUzunluk)
+# # print('İsminde ' + isimUzunluk_Str + ' karakter var.')
+# ikihanelisayı = str(input('İki haneli bir sayı giriniz '))
+# toplam = int(ikihanelisayı[0]) + (int(ikihanelisayı[0]))
+# # print('Toplam:' + str(toplam))
+# # print(4 * 8 / 2)
+# Boy = float(input('Boyunuzu giriniz: '))
+# Kilo = float(input('Kilonuzu giriniz: '))
+# vki = int(Kilo/Boy**2)
+# print('Vücut Kilo Endeksiniz: ' + str(vki))
+# print(8/3)
+# print(type(8/3))
+# # print(type)(8//3)
+# Yaş = input('Kaç yaşındasın?')
+# Yaş_int = int(Yaş)
+# Ay = Yaş_int * 12
+# Hafta = Yaş_int * 52
+# Gün = Yaş_int * 365
+# print(f'{Ay} aylıksın')
+# print(f'{Gün} günlüksün')
+# print(f'{Hafta} haftalıksın')
+# print('Bahşiş hesaplayıcıya hoşgeldiniz!')   
+# Hesap = float(input('Hesap ne geldi?'))
+# KişiSayısı = int(input('kaç kişisiniz?'))
+# Oran = float(input('Bahşiş oranı ne olsun?'))
+# Toplam = Hesap + (Hesap * Oran * 100)
+# KişiBaşı = Toplam / KişiSayısı
+# print(f'Kişi başı ödemeniz gerekn tutar = {KişiSayısı} TL')
+# if bla blaysa al 
+# elif bla bla değilse al 
+# else ikisi de değilse al vb. 
+print('Lunaparka hoşgeldiniz!')
+print('Bilet fiyatı yetişkin 20 TL. 12 yaşından küçükler 10 TL. ')
+Boy = int(input('Lütfen boyunuzu giriniz (cm)'))
+Yaş = int(input('Lütfen yaşınızı giriniz'))
+BiletFiyatı = 0
+if Boy > 80
