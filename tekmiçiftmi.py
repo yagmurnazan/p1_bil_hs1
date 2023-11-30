c9ea9653360a35f1047ed0370f234e844429ea0b
yagmurnazan <147739421+yagmurnazan@users.noreply.github.com>
2023-11-30 18:48:09 +0300

    adfs

--- tekmiçiftmi.py
+++ tekmiçiftmi.py
@@ -3,4 +3,5 @@ Sayı = int(input("Bir sayı giriniz"))
 if Sayı %2 == 0:
     print('Girdiğiniz sayı çifttir.')
 else:
-    print('Girdiğiniz sayı tektir.')
+    print('Girdiğiniz sayı tektir.') 
+    #asdöjbcsödbjvadö   
